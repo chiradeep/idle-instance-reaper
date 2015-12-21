579a5b6cfdad3feb27c9bbd2098a72d382e6ee35
Chiradeep Vittal <chiradeep@apache.org>
2015-12-21 15:18:07 -0800

    Script to automate creation of rule and upload of function

--- lambda/create-role-func.sh
+++ lambda/create-role-func.sh
@@ -9,10 +9,25 @@ role_arn=$(aws iam create-role --role-name=$ROLE_NAME \
                                --output text \
                                --query 'Role.Arn')
 
+sleep 3
 policy_arn=$(aws iam create-policy --policy-name=$POLICY_NAME \
                                    --policy-document=file://idle-instance-stop-policy.json \
                                    --output text \
                                    --query 'Policy.Arn')
 
+sleep 3
 aws iam attach-role-policy --role-name=$ROLE_NAME --policy-arn=$policy_arn
 
+sleep 3
+fname=idle-instance-lambda
+
+zip -r $fname.zip $fname.py
+
+aws lambda create-function \
+  --function-name "$fname" \
+  --zip-file fileb://"$fname.zip" \
+  --role "$role_arn" \
+  --handler "$fname.lambda_handler" \
+  --timeout 30 \
+  --runtime python2.7 \
+  --description "Stops idle instances"
